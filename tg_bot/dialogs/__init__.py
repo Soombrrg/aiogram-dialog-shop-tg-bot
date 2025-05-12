9a322c3bb176e1189b365b67ccb475cc3f3807be
Soombrrg <slav.volchanin@mail.ru>
2025-05-12 15:01:48 +0300

    fix removed unused

--- tg_bot/dialogs/__init__.py
+++ tg_bot/dialogs/__init__.py
@@ -1,10 +1,9 @@
 from aiogram import Dispatcher
-from aiogram_dialog.setup import DialogRegistry, setup_dialogs
+from aiogram_dialog.setup import setup_dialogs
 from . import bot_menu
 
 
 def setup_dialogs2(dp: Dispatcher):
-    registry = DialogRegistry(dp)
     for dialog in [*bot_menu.bot_menu_dialogs()]:
         dp.include_router(dialog)
 
